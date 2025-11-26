66414cc36965c132ca8d720361f2f1378dd07404
SleepyyNet <100797927+SleepyyDotNet@users.noreply.github.com>
2025-11-26 10:48:26 +0100

    Update FlatMMO.Com-By-SleepyyNet.js
    Changing the links out to make them correct.

--- FlatMMO.Com-By-SleepyyNet.js
+++ FlatMMO.Com-By-SleepyyNet.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         FlatMMO Play – Overview Style Button
-// @namespace    http://tampermonkey.net/
+// @name         FlatMMO Play – Overview Style Button by SleepyyNet
+// @namespace    https://github.com/SleepyyDotNet/FlatMMO-Scripts/blob/greasyfork/FlatMMO.Com-By-SleepyyNet.js
 // @version      1.2
 // @description  Adds an overview-style Play button for FlatMMO + credits, with optional auto-redirect.
 // @match        *://flatmmo.com/*
