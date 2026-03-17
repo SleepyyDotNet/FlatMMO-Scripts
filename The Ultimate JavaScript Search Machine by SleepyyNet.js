05aeb2d28a4bc8beeb485c3caae59e8d12bdf164
SleepyyNet <100797927+SleepyyDotNet@users.noreply.github.com>
2026-03-17 14:37:56 +0100

    Add multilingual support for script metadata

--- The Ultimate JavaScript Search Machine by SleepyyNet.js
+++ The Ultimate JavaScript Search Machine by SleepyyNet.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name            The Ultimate JavaScript Search Machine by SleepyyNet
-// @namespace       http://tampermonkey.net/
+// @name:en         The Ultimate JavaScript Search Machine by SleepyyNet
+// @name:se         Den ultimata JavaScript-sökmaskinen av SleepyyNet
+// @namespace:en    http://tampermonkey.net/
+// @namespace:se    http://tampermonkey.net/
 // @version         1.1
-// @description:se  Ett kraftfullt metasök-verktyg som söker igenom alla stora arkiv för användarskript (Greasy Fork, Sleazy Fork, OpenUserJS, Userscript.Zone, GitHub & Gist) samtidigt.
 // @description:en  A powerful metasearch tool that searches all major userscript repositories (Greasy Fork, Sleazy Fork, OpenUserJS, Userscript.Zone, GitHub & Gist) simultaneously.
+// @description:se  Ett kraftfullt metasök-verktyg som söker igenom alla stora arkiv för användarskript (Greasy Fork, Sleazy Fork, OpenUserJS, Userscript.Zone, GitHub & Gist) samtidigt.
 // @author          SleepyyNet
 // @match           *://*/*
 // @include         *
